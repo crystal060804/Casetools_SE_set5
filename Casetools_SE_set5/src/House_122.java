4da1d4886895e76f656edc2b670be75a0ca1644e
exam <exam@Bcalab4-m23>
2024-04-27 09:28:38 +0530

    commit

---
+++ Casetools_SE_set5/src/House_122.java
@@ -0,0 +1,25 @@
+
+public class House_122 {
+
+	public class House{
+		String color;
+		String Owner;
+		
+		House(String color, String owner)
+		{
+			this.color=color;
+			this.Owner=owner;
+		}
+		
+		String getOwner()
+		{
+			return Owner;
+		}
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
